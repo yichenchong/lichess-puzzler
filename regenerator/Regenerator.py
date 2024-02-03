cd5b75952141994e16445dcf92efc641f5b61f15
hw1621 <hw1621@ic.ac.uk>
2024-02-03 22:44:18 +0000

    implement generate_new_puzzle to generate similar puzzles by moving pieces

--- regenerator/Regenerator.py
+++ regenerator/Regenerator.py
@@ -21,8 +21,8 @@ version = 48
 logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s %(levelname)-4s %(message)s', datefmt='%m/%d %H:%M')
 
-pair_limit = chess.engine.Limit(depth = 50, time = 30, nodes = 30_000_000)
-mate_defense_limit = chess.engine.Limit(depth = 15, time = 10, nodes = 10_000_000)
+pair_limit = chess.engine.Limit(depth=50, time=30, nodes=30_000_000)
+mate_defense_limit = chess.engine.Limit(depth=15, time=10, nodes=10_000_000)
 
 mate_soon = Mate(15)
 
@@ -41,20 +41,20 @@ class Regenerator:
             # if there's more than one mate in one, gotta look if the best non-mating move is bad enough
             logger.debug('Looking for best non-mating move...')
             mates = count_mates(copy.deepcopy(pair.node.board()))
-            info = self.engine.analyse(pair.node.board(), multipv = mates + 1, limit = pair_limit)
-            scores =  [pv["score"].pov(pair.winner) for pv in info]
+            info = self.engine.analyse(pair.node.board(), multipv=mates + 1, limit=pair_limit)
+            scores = [pv["score"].pov(pair.winner) for pv in info]
             # the first non-matein1 move is the last element
             if scores[-1] < Mate(1) and win_chances(scores[-1]) > non_mate_win_threshold:
-                    return False
+                return False
             return True
         return False
 
     # is pair.best the only continuation?
     def is_valid_attack(self, pair: NextMovePair) -> bool:
         return (
-            pair.second is None or
-            self.is_valid_mate_in_one(pair) or
-            win_chances(pair.best.score) > win_chances(pair.second.score) + 0.7
+                pair.second is None or
+                self.is_valid_mate_in_one(pair) or
+                win_chances(pair.best.score) > win_chances(pair.second.score) + 0.7
         )
 
     def get_next_pair(self, node: ChildNode, winner: Color) -> Optional[NextMovePair]:
@@ -65,7 +65,7 @@ class Regenerator:
         return pair
 
     def get_next_move(self, node: ChildNode, limit: chess.engine.Limit) -> Optional[Move]:
-        result = self.engine.play(node.board(), limit = limit)
+        result = self.engine.play(node.board(), limit=limit)
         return result.move if result else None
 
     def cook_mate(self, node: ChildNode, winner: Color) -> Optional[List[Move]]:
@@ -96,7 +96,6 @@ class Regenerator:
 
         return [move] + follow_up
 
-
     def cook_advantage(self, node: ChildNode, winner: Color) -> Optional[List[NextMovePair]]:
 
         board = node.board()
@@ -119,52 +118,56 @@ class Regenerator:
 
         return [pair] + follow_up
 
-
     def generate_new_puzzle(self, puzzle: Puzzle) -> Union[List[Puzzle], None]:
         # create list of potentially removable pieces
         # try removing each piece
         # test each piece with analyze_position
         # compare main lines with the resultant puzzles, if equal, add to the list
         # return the list
-
-        white_pieces = {'R', 'N', 'B', 'Q', 'P'}
-        black_pieces = {'r', 'n', 'b', 'q', 'p'}
-        # get the Fen of the puzzle root node
+        puzzles = []
         current_board = puzzle.node.board()
+        dict_of_pieces = current_board.piece_map(mask=chess.BB_ALL)
+        is_previous_checked = current_board.is_check()
 
-        fen_of_puzzle_origin = puzzle.node.board().fen()
-        removable_pieces = fen_of_puzzle_origin.split("/")
-        # ['rnbqk1nr', 'pppppppp', '8', '8', '8', '8', 'PPPPPPPP', 'RNBQK1NR']
-        for pieces in removable_pieces:
-            if pieces == "8":
+        for index, piece in dict_of_pieces.items():
+            if piece.symbol() == 'K' or piece.symbol() == 'k':
                 continue
-            for piece in pieces:
-                if piece == 'k' or piece == 'K':
-                    continue
+            current_board.remove_piece_at(index)
+            if current_board.is_checkmate() \
+                    or current_board.is_stalemate() \
+                    or (is_previous_checked and not current_board.is_check()):
+                current_board.set_piece_at(index, piece)
+                continue
+            new_fen = current_board.fen()
+            new_game = chess.pgn.Game()
+            new_game.setup(chess.Board(new_fen))
 
+            info = self.engine.analyse(current_board, chess.engine.Limit(depth=20))
+            current_eval = info["score"]
+            new_puzzle = self.analyze_position(new_game, current_eval, tier=10)
 
+            if isinstance(new_puzzle, Score):
+                continue
+            else:
+                new_moves = new_puzzle.moves
+                mainlines = new_game.mainline_moves()
+                if len(mainlines) != len(new_moves): continue
+                is_same = True
+                for i in range(0, len(new_moves)) :
+                    if new_moves[i] != mainlines[i]:
+                        is_same = False
+                        break
+                if is_same:
+                    puzzles.append(new_puzzle)
+        if len(puzzles) == 0:
+            return None
+        return puzzles
 
-
-        # Remove the possible pieces of the original fen
-        # TODO:
-        fen_of_puzzle_modified = fen_of_puzzle_origin
-
-
-        new_game = chess.pgn.Game()
-        new_board = chess.Board(fen_of_puzzle_modified)
-        new_game.setup(new_board)
-        prev_score = puzzle.cp
-
-
-        new_puzzle = self.analyze_position(new_game, prev_score, tier=10)
-
-
-    def analyze_position(self, node: GameNode, prev_score: Score, current_eval: PovScore, tier: int) -> Union[Puzzle, Score]:
+    def analyze_position(self, node: GameNode, current_eval: PovScore, tier: int) -> Union[Puzzle, Score]:
 
         board = node.board()
         winner = board.turn
         score = current_eval.pov(winner)
-
         if board.legal_moves.count() < 2:
             return score
 
@@ -172,9 +175,6 @@ class Regenerator:
 
         logger.debug("{} {} to {}".format(node.ply(), node.move.uci() if node.move else None, score))
 
-        if prev_score > Cp(300) and score < mate_soon:
-            logger.debug("{} Too much of a winning position to start with {} -> {}".format(node.ply(), prev_score, score))
-            return score
         if is_up_in_material(board, winner):
             logger.debug("{} already up in material {} {} {}".format(node.ply(), winner, material_count(board, winner), material_count(board, not winner)))
             return score
@@ -190,16 +190,16 @@ class Regenerator:
             if mate_solution is None or (tier == 1 and len(mate_solution) == 3):
                 return score
             return Puzzle(node, mate_solution, 999999999)
-        elif score >= Cp(200) and win_chances(score) > win_chances(prev_score) + 0.6:
+        elif score >= Cp(200): # and win_chances(score) > win_chances(prev_score) + 0.6:
             if score < Cp(400) and material_diff(board, winner) > -1:
                 logger.debug("Not clearly winning and not from being down in material, aborting")
                 return score
-            logger.debug("Advantage {}#{} {} -> {}. Probing...".format(game_url, node.ply(), prev_score, score))
+            logger.debug("Advantage {}#{} {} -> {}. Probing...".format(game_url, node.ply(), score))
             if self.server.is_seen_pos(node):
                 logger.debug("Skip duplicate position")
                 return score
             puzzle_node = copy.deepcopy(node)
-            solution : Optional[List[NextMovePair]] = self.cook_advantage(puzzle_node, winner)
+            solution: Optional[List[NextMovePair]] = self.cook_advantage(puzzle_node, winner)
             self.server.set_seen(node.game())
             if not solution:
                 return score
@@ -207,7 +207,7 @@ class Regenerator:
                 if not solution[-1].second:
                     logger.debug("Remove final only-move")
                 solution = solution[:-1]
-            if not solution or len(solution) == 1 :
+            if not solution or len(solution) == 1:
                 logger.debug("Discard one-mover")
                 return score
             if tier < 3 and len(solution) == 3:
@@ -248,88 +248,96 @@ def open_file(file: str):
     return open(file)
 
 def main() -> None:
-    board = chess.Board("rnbqkbnr/pp1p2pp/2p2p2/3Pp3/4P3/8/PPP2PPP/RNBQKBNR w KQkq e6 0 4")
+    board = chess.Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
     map = board.piece_map(mask=chess.BB_ALL)
+    cur_fen = board.fen()
+    board.remove_piece_at(0)
+    board.remove_piece_at(63)
+    new_fen = board.fen()
+    print("old", cur_fen)
+    print("new", new_fen)
+
     print(map)
-    sys.setrecursionlimit(10000) # else node.deepcopy() sometimes fails?
-    args = parse_args()
-    if args.verbose == 2:
-        logger.setLevel(logging.DEBUG)
-    else:
-        logger.setLevel(logging.INFO)
-    engine = make_engine(args.engine, args.threads)
-    server = Server(logger, args.url, args.token, version)
-    generator = Regenerator(engine, server)
-    games = 0
-    site = "?"
-    has_master = False
-    tier = 0
-    skip = int(args.skip)
-    logger.info("Skipping first {} games".format(skip))
+    # sys.setrecursionlimit(10000) # else node.deepcopy() sometimes fails?
+    # args = parse_args()
+    # if args.verbose == 2:
+    #     logger.setLevel(logging.DEBUG)
+    # else:
+    #     logger.setLevel(logging.INFO)
+    # engine = make_engine(args.engine, args.threads)
+    # server = Server(logger, args.url, args.token, version)
+    # generator = Regenerator(engine, server)
+    # games = 0
+    # site = "?"
+    # has_master = False
+    # tier = 0
+    # skip = int(args.skip)
+    # logger.info("Skipping first {} games".format(skip))
+    #
+    # parts = int(args.parts)
+    # part = int(args.part)
+    # print(f'v{version} {args.file} {part}/{parts}')
+    #
+    # try:
+    #     with open_file(args.file) as pgn:
+    #         skip_next = False
+    #         for line in pgn:
+    #             if line.startswith("[Site "):
+    #                 site = line
+    #                 games = games + 1
+    #                 has_master = False
+    #                 tier = 4
+    #             elif games < skip:
+    #                 continue
+    #             elif games % parts != part - 1:
+    #                 continue
+    #             if tier == 0:
+    #                 skip_next = True
+    #             elif line.startswith("[Variant ") and not line.startswith("[Variant \"Standard\"]"):
+    #                 skip_next = True
+    #             elif (
+    #                     (line.startswith("[WhiteTitle ") or line.startswith("[BlackTitle ")) and
+    #                     "BOT" not in line
+    #                 ):
+    #                 has_master = True
+    #             else:
+    #                 r_tier = util.rating_tier(line)
+    #                 t_tier = util.time_control_tier(line)
+    #                 if r_tier is not None:
+    #                     tier = min(tier, r_tier)
+    #                 elif t_tier is not None:
+    #                     tier = min(tier, t_tier)
+    #                 elif line.startswith("1. ") and skip_next:
+    #                     logger.debug("Skip {}".format(site))
+    #                     skip_next = False
+    #                 elif "%eval" in line:
+    #                     tier = tier + 1 if has_master else tier
+    #                     game = chess.pgn.read_game(StringIO("{}\n{}".format(site, line)))
+    #                     assert(game)
+    #                     nb_moves = len(list(game.mainline_moves()))
+    #                     tier = tier + 1 if nb_moves < 38 else tier
+    #                     tier = tier + 1 if nb_moves < 21 else tier
+    #                     game_id = game.headers.get("Site", "?")[20:]
+    #                     if server.is_seen(game_id):
+    #                         to_skip = 1
+    #                         logger.info(f'Game {game_id} was already seen before, skipping {to_skip} - {games}')
+    #                         skip = games + to_skip
+    #                         continue
+    #
+    #                     # logger.info(f'https://lichess.org/{game_id} tier {tier}')
+    #                     try:
+    #                         puzzle = generator.analyze_game(game, tier)
+    #                         if puzzle is not None:
+    #                             logger.info(f'v{version} {args.file} {part}/{parts} {util.avg_knps()} knps, tier {tier}, game {games}')
+    #                             server.post(game_id, puzzle)
+    #                     except Exception as e:
+    #                         logger.error("Exception on {}: {}".format(game_id, e))
+    # except KeyboardInterrupt:
+    #     print(f'v{version} {args.file} Game {games}')
+    #     sys.exit(1)
+    #
+    # engine.close()
 
-    parts = int(args.parts)
-    part = int(args.part)
-    print(f'v{version} {args.file} {part}/{parts}')
-
-    try:
-        with open_file(args.file) as pgn:
-            skip_next = False
-            for line in pgn:
-                if line.startswith("[Site "):
-                    site = line
-                    games = games + 1
-                    has_master = False
-                    tier = 4
-                elif games < skip:
-                    continue
-                elif games % parts != part - 1:
-                    continue
-                if tier == 0:
-                    skip_next = True
-                elif line.startswith("[Variant ") and not line.startswith("[Variant \"Standard\"]"):
-                    skip_next = True
-                elif (
-                        (line.startswith("[WhiteTitle ") or line.startswith("[BlackTitle ")) and
-                        "BOT" not in line
-                    ):
-                    has_master = True
-                else:
-                    r_tier = util.rating_tier(line)
-                    t_tier = util.time_control_tier(line)
-                    if r_tier is not None:
-                        tier = min(tier, r_tier)
-                    elif t_tier is not None:
-                        tier = min(tier, t_tier)
-                    elif line.startswith("1. ") and skip_next:
-                        logger.debug("Skip {}".format(site))
-                        skip_next = False
-                    elif "%eval" in line:
-                        tier = tier + 1 if has_master else tier
-                        game = chess.pgn.read_game(StringIO("{}\n{}".format(site, line)))
-                        assert(game)
-                        nb_moves = len(list(game.mainline_moves()))
-                        tier = tier + 1 if nb_moves < 38 else tier
-                        tier = tier + 1 if nb_moves < 21 else tier
-                        game_id = game.headers.get("Site", "?")[20:]
-                        if server.is_seen(game_id):
-                            to_skip = 1
-                            logger.info(f'Game {game_id} was already seen before, skipping {to_skip} - {games}')
-                            skip = games + to_skip
-                            continue
-
-                        # logger.info(f'https://lichess.org/{game_id} tier {tier}')
-                        try:
-                            puzzle = generator.analyze_game(game, tier)
-                            if puzzle is not None:
-                                logger.info(f'v{version} {args.file} {part}/{parts} {util.avg_knps()} knps, tier {tier}, game {games}')
-                                server.post(game_id, puzzle)
-                        except Exception as e:
-                            logger.error("Exception on {}: {}".format(game_id, e))
-    except KeyboardInterrupt:
-        print(f'v{version} {args.file} Game {games}')
-        sys.exit(1)
-
-    engine.close()
 
 if __name__ == "__main__":
     main()
